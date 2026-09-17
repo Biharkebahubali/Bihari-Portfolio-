4dbf124b121af93932fc52d2deb1fa07d0f1b7bd
VIKRAM KUMAR <vikramkumarart@gmail.com>
2026-09-17 11:05:42 +0530

    Add files via upload

--- src/config/sanity.js
+++ src/config/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 import { createImageUrlBuilder } from '@sanity/image-url';
 
 export const sanityClient = createClient({
-    projectId: 'kv5wjjmj', // Zostanie uzupełnione po utworzeniu projektu w Sanity
+    projectId: 'ae68by6m', // Zostanie uzupełnione po utworzeniu projektu w Sanity
     dataset: 'production',
     useCdn: true, // `false` dla środowiska dev, `true` dla produkcji żeby było szybciej
     apiVersion: '2024-03-01', // aktualna data API
